dc82382d27cfe5d48cb1f83f8a0de7613da76461
Esteban <esteban.racana@gmail.com>
2026-02-17 19:00:34 -0300

    Update!
    Changes images to load on the program and eliminate variables

--- dance-start.py
+++ dance-start.py
@@ -4,22 +4,20 @@ height=600
 WHITE=(0,0,0)
 pygame.init()
 screen=pygame.display.set_mode((width,height))
-background=pygame.image.load("background.png").convert()
+background=pygame.image.load("background.png").convert_alpha()
 background=pygame.transform.scale(background,(width,height))
+background.set_alpha(128)
 clock=pygame.time.Clock()
 state="menu"
 done=False
 def begin_start():
-    screen.fill(WHITE)
-    font=pygame.font.SysFont("Arial",50)
-    title=font.render("Dance-Start",True,(255,255,255))
-    screen.blit(title,(250,200))
-
-    small_font=pygame.font.SysFont("Arial",30)
-    message=small_font.render("Presiona ESPACIO para continuar",True,(180,180,180))
+    screen.blit(background,(0,0))
+    small_font=pygame.font.SysFont("Arial",32)
+    message=small_font.render("Presiona ESPACIO para continuar",True,(255,255,255))
     message_rect=message.get_rect(center=(width/2,height/2+100))    
     screen.blit(message,(200,350))
 
+    
 def start_game():
     screen.fill(WHITE)
     font=pygame.font.SysFont("Arial",50)
